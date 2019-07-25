c101a14b35c77df0ca2331079b3180f9c22536b9
Nabid Hasan Sunny <nbdsunny@gmail.com>
2019-07-25 22:36:39 +0600

    folder browsing feature adde. Home button added.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-if(isset($_POST['file_path'])) {
-    $filePath = $_POST['file_path'];
-    if (file_exists($filePath)) {
+if(isset($_GET['file_path'])) {
+    $filePath = $_GET['file_path'];
+    if(is_dir($filePath)) {
+        $directory = $filePath;
+    } else if (file_exists($filePath)) {
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename="'.basename($filePath).'"');
@@ -14,17 +16,27 @@ if(isset($_POST['file_path'])) {
     }
 }
 
-$link =  'shortcut.lnk';
-$data = file_get_contents($link);
-$dir = preg_replace('@^.*\00([A-Z]:)(?:[\00\\\\]|\\\\.*?\\\\\\\\.*?\00)([^\00]+?)\00.*$@s', '$1\\\\$2', $data);
-$files = scandir($dir);
+$data = file_get_contents('shortcut.lnk');
+$homeDirectory = preg_replace('@^.*\00([A-Z]:)(?:[\00\\\\]|\\\\.*?\\\\\\\\.*?\00)([^\00]+?)\00.*$@s', '$1\\\\$2', $data);
+
+if(!isset($directory)) {
+    $directory = $homeDirectory;
+}
+
+$files = scandir($directory);
 
 ?>
-<form action="index.php" method="POST">
+<form action="index.php" method="GET">
+    <input type="submit" value="<?php echo $homeDirectory ?>" name="file_path"><hr>
+</form>
+
+<form action="index.php" method="GET">
 <?php
 foreach($files as $file) {
-    $filePath = $dir.'\\'.$file;
-    echo '<input type="submit" value="'.$filePath.'" name="file_path"><br><br>';
+    if($file != "." && $file != "..") {
+        $filePath = $directory.'\\'.$file;
+        echo '<input type="submit" value="'.$filePath.'" name="file_path"><br><br>';
+    }
 }
 ?>
 </form>
